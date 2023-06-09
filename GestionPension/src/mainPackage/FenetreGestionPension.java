88672eab3e6eada15a4b90b25cac9411867a11f0
Jossuc4 <larryjossuc4@gmail.com>
2023-06-09 18:33:46 +0300

    Gestion tarifs

--- GestionPension/src/mainPackage/FenetreGestionPension.java
+++ GestionPension/src/mainPackage/FenetreGestionPension.java
@@ -25,11 +25,25 @@ import java.awt.FlowLayout;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import java.awt.BorderLayout;
+import javax.swing.JTabbedPane;
+import java.awt.Dimension;
+import javax.swing.border.LineBorder;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.event.ChangeListener;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.JScrollPane;
 
 public class FenetreGestionPension extends JFrame {
 
 	private JPanel contentPane;
-	private JTable table_1;
+	private JTextField txtCatgorie;
+	private JTextField txtMontant;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTable listeTarifs;
 
 	/**
 	 * Launch the application.
@@ -60,153 +74,145 @@ public class FenetreGestionPension extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		contentPane.setLayout(new BorderLayout(0,10));
+		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.X_AXIS));
 		
-		JPanel bigTitle = new JPanel();
-		bigTitle.setBackground(new Color(255, 255, 255));
-		contentPane.add(bigTitle);
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		
-		JPanel navigation = new JPanel();
-		navigation.setBackground(new Color(255, 255, 255));
-		contentPane.add(navigation);
-		navigation.setLayout(null);
+		contentPane.add(tabbedPane);
 		
-		JPanel toolBar = new JPanel();
-		toolBar.setBackground(new Color(255, 255, 255));
-		contentPane.add(toolBar);
-		toolBar.setLayout(null);
+		JPanel panel_2 = new JPanel();
+		tabbedPane.addTab("New tab", null, panel_2, null);
 		
-		JButton btnAddButton = new JButton("Ajouter");
+		JPanel Tarif = new JPanel();
+		Tarif.setToolTipText("");
+		Tarif.setName("");
+		Tarif.setPreferredSize(new Dimension(1000, 500));
+		Tarif.setMinimumSize(new Dimension(20, 10));
+		Tarif.setBackground(new Color(255, 255, 255));
+		tabbedPane.addTab("Tarif", null, Tarif, null);
+		Tarif.setLayout(null);
 		
-		btnAddButton.setFont(new Font("Times New Roman", Font.BOLD, 12));
-		btnAddButton.setForeground(new Color(0, 0, 0));
-		btnAddButton.setBackground(new Color(192, 192, 192));
-		btnAddButton.setBounds(10, 11, 89, 23);
-		toolBar.add(btnAddButton);
+		JPanel panel = new JPanel();
+		panel.setBorder(new LineBorder(Color.DARK_GRAY));
+		panel.setBounds(25, 59, 386, 538);
+		Tarif.add(panel);
+		panel.setLayout(null);
 		
-		JButton btnFindButton = new JButton("Chercher");
+		JLabel formTitleLabel = new JLabel("CREATION");
+		formTitleLabel.setFont(new Font("Segoe UI Symbol", Font.BOLD | Font.ITALIC, 25));
+		formTitleLabel.setBounds(118, 11, 140, 41);
+		panel.add(formTitleLabel);
 		
-		btnFindButton.setFont(new Font("Times New Roman", Font.BOLD, 12));
-		btnFindButton.setForeground(new Color(0, 0, 0));
-		btnFindButton.setBackground(new Color(192, 192, 192));
-		btnFindButton.setBounds(164, 11, 89, 23);
-		toolBar.add(btnFindButton);
+		txtCatgorie = new JTextField();
+		txtCatgorie.setText("Catégorie");
+		txtCatgorie.setColumns(10);
+		txtCatgorie.setBounds(34, 131, 300, 20);
+		panel.add(txtCatgorie);
+		
+		txtMontant = new JTextField();
+		txtMontant.setText("Montant");
+		txtMontant.setColumns(10);
+		txtMontant.setBounds(34, 175, 300, 20);
+		panel.add(txtMontant);
+		
+		JButton btnValidTarif = new JButton("Valider");
+		btnValidTarif.setBounds(245, 229, 89, 23);
+		panel.add(btnValidTarif);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"M", "N", "O", "P", "I", "J", "K", "L"}));
+		comboBox.setBounds(140, 84, 194, 22);
+		panel.add(comboBox);
+		
+		JLabel comboDiplTarif = new JLabel("Diplôme: ");
+		comboDiplTarif.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		comboDiplTarif.setBounds(34, 84, 83, 22);
+		panel.add(comboDiplTarif);
+		
+		JButton btnValidTarif_1 = new JButton("Valider");
+		btnValidTarif_1.setBounds(245, 479, 89, 23);
+		panel.add(btnValidTarif_1);
+		
+		textField = new JTextField();
+		textField.setText("Montant");
+		textField.setColumns(10);
+		textField.setBounds(34, 425, 300, 20);
+		panel.add(textField);
+		
+		textField_1 = new JTextField();
+		textField_1.setText("Catégorie");
+		textField_1.setColumns(10);
+		textField_1.setBounds(34, 381, 300, 20);
+		panel.add(textField_1);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setBounds(140, 334, 194, 22);
+		panel.add(comboBox_1);
+		
+		JLabel comboDiplTarif_1 = new JLabel("Diplôme: ");
+		comboDiplTarif_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		comboDiplTarif_1.setBounds(34, 334, 83, 22);
+		panel.add(comboDiplTarif_1);
+		
+		JLabel lblModification = new JLabel("MODIFICATION");
+		lblModification.setFont(new Font("Segoe UI Symbol", Font.BOLD | Font.ITALIC, 25));
+		lblModification.setBounds(86, 279, 204, 41);
+		panel.add(lblModification);
+		
+		JLabel tarifLabel = new JLabel("GESTION DE TARIFS");
+		tarifLabel.setFont(new Font("Segoe UI Semibold", Font.BOLD | Font.ITALIC, 25));
+		tarifLabel.setForeground(new Color(0, 0, 0));
+		tarifLabel.setBounds(433, 11, 250, 41);
+		Tarif.add(tarifLabel);
 		
 		JPanel ListPanel = new JPanel();
-		contentPane.add(ListPanel);
+		ListPanel.setBounds(464, 59, 495, 538);
+		Tarif.add(ListPanel);
 		
-		JLabel ListIm = new JLabel("Immatriculation");
+		//-----------------Events
 		
-		JLabel ListNom = new JLabel("Nom");
-		
-		JLabel ListPrenoms = new JLabel("Prénoms");
-		
-		JLabel ListDatenais = new JLabel("Date de naissance");
-		
-		table_1 = new JTable();
-		table_1.setToolTipText("Nom");
-		table_1.setBorder(null);
-		GroupLayout gl_ListPanel = new GroupLayout(ListPanel);
-		gl_ListPanel.setHorizontalGroup(
-			gl_ListPanel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_ListPanel.createSequentialGroup()
-					.addGap(41)
-					.addGroup(gl_ListPanel.createParallelGroup(Alignment.TRAILING, false)
-						.addComponent(table_1, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addGroup(Alignment.LEADING, gl_ListPanel.createSequentialGroup()
-							.addComponent(ListIm)
-							.addGap(18)
-							.addComponent(ListNom)
-							.addGap(18)
-							.addComponent(ListPrenoms)
-							.addGap(18)
-							.addComponent(ListDatenais)))
-					.addGap(255))
-		);
-		gl_ListPanel.setVerticalGroup(
-			gl_ListPanel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_ListPanel.createSequentialGroup()
-					.addGap(5)
-					.addGroup(gl_ListPanel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(ListIm)
-						.addComponent(ListNom)
-						.addComponent(ListPrenoms)
-						.addComponent(ListDatenais))
-					.addGap(29)
-					.addComponent(table_1, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(110, Short.MAX_VALUE))
-		);
-		ListPanel.setLayout(gl_ListPanel);
-		
-		/*-----------------------------------Events----------------------------*/
-		
-		
-		//btnAdd clicked
-		btnAddButton.addMouseListener(new MouseAdapter() {
-			private boolean btnAddClicked=false;
-			//Add event on the pension button
-			public void mouseClicked(MouseEvent e) {
-				if(!btnAddClicked) {
-					btnAddClicked=true;
-				}else {
-					btnAddClicked=false;
-				}
-				btnAddButton.setBackground(new Color(0, 64, 128));
-				btnFindButton.setBackground(new Color(192, 192, 192));
-				
-				DatabaseConnect postgresDB=new DatabaseConnect();
-				Connection connect=postgresDB.connect();
-				Object[][] dataSet=new Object [][]{{}};
-				int x=0;
-				
-				try {
-					Statement query=connect.createStatement();
-					ResultSet data= query.executeQuery("SELECT * FROM personne");
-			        String[] columns = new String[] {
-			                "Id", "Nom", "Adresse", "Taux horaire", "A temps partiel"
-			            };
-			             
-			            //données pour JTable dans un tableau 2D
-			            Object[][] dataFetched = new Object[][] {
-			                {1, "Thomas", "Paris", 20.0, true },
-			                {2, "Jean", "Marseille", 50.0, false },
-			                {3, "Yohan", "Lyon", 30.0, true },
-			                {4, "Emily", "Toulouse", 60.0, false },
-			                {5, "Alex", "Nice", 10.0, false },
-			            };
-			            JTable table=new JTable(dataFetched,columns);
-			            ListPanel.add(table);
-					while(data.next()) {
+		tabbedPane.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				if(tabbedPane.getSelectedIndex()==1) {
+					System.out.println("Tab changed\n");
+					DatabaseConnect cnx=new DatabaseConnect();
+					
+					try {
+						Connection connection=cnx.connect();
+						Statement query=connection.createStatement();
+						ResultSet rs=query.executeQuery("SELECT * FROM public.tarif");
+						ResultSetMetaData tarifs=rs.getMetaData();
 						
-						JLabel nom=new JLabel("Hey");
-						nom.setBackground(Color.yellow);
-						nom.setBounds(330, 23, 93, 14);
-						nom.setForeground(Color.BLUE);
-						ListPanel.add(nom);
-						System.out.println("Immatriculation: "+data.getString("IM")+"\n"+data.getString("datenais"));
+						int columns=tarifs.getColumnCount();
+						String [] tableHeader=new String[columns];
+						
+						DefaultTableModel model=new DefaultTableModel();
+						
+						for(int i=0;i<columns;i++) {
+							tableHeader[i]=tarifs.getColumnName(i+1);
+							System.out.println(tableHeader[i]);
+						}
+						model.setColumnIdentifiers(tableHeader);
+					
+						
+						while (rs.next()) {
+			                Object[] rowData = new Object[columns];
+			                for (int i = 1; i <= columns; i++) {
+			                    rowData[i - 1] = rs.getObject(i);
+			                }
+			                model.addRow(rowData);
+			            }
+						JTable ListTable=new JTable(model);
+						JScrollPane scrollpane=new JScrollPane(ListTable);
+						
+						ListPanel.add(scrollpane);
+					}catch(Exception error) {
+						System.out.println(error);
 					}
-				}catch(Exception error) {
-					System.out.println(error.getMessage());
 				}
-				
 				
 			}
 			
-			
-		});
-		
-		//btnFind clicked
-		btnFindButton.addMouseListener(new MouseAdapter() {
-			private boolean btnFindClicked=false; 
-			public void mouseClicked(MouseEvent e) {
-				if(!btnFindClicked) {
-					btnFindClicked=true;
-				}else{
-					btnFindClicked=false;
-				}
-				btnFindButton.setBackground(new Color(0, 64, 128));
-				btnAddButton.setBackground(new Color(192, 192, 192));
-			}
 		});
 	}
 }
